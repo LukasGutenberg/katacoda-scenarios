91cf82c1d18078d0d48637f383c631e09314d7a2
Autonymic <75275746+Autonymic@users.noreply.github.com>
2022-05-15 15:21:40 +0200

    fix: Reverting foreground.sh

--- basic-cue-tutorial/foreground.sh
+++ basic-cue-tutorial/foreground.sh
@@ -1,14 +1,5 @@
 #/bin/bash
 
-echo "Downloading golang"
-wget https://go.dev/dl/go1.18.2.linux-amd64.tar.gz
-
-echo "Extracting golang"
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.2.linux-amd64.tar.gz
-
-echo "Installing cuelang with go"
-go install cuelang.org/go/cmd/cue@latest
-
 printf 'This is a bash shell script.\n\n'
 printf 'Any commands you put in here will be executed at runtime.\n\n'
 printf 'As such, this is a good place to install and configure anything you want to have ready for the learner before they get started.\n\n'

276ad59447e08de795008af23c4488173ec2d346
Autonymic <75275746+Autonymic@users.noreply.github.com>
2022-05-16 14:44:55 +0200

    Moving installation from background to foreground

--- basic-cue-tutorial/foreground.sh
+++ basic-cue-tutorial/foreground.sh
@@ -1,5 +1,12 @@
 #/bin/bash
-echo "wait until the file explorer stops updating before you begin running commands!\n"
+
+echo "Preparing the environment. Wait for the file explorer to stop updating before you begin running commands!\n"
+
+wget https://go.dev/dl/go1.18.2.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.2.linux-amd64.tar.gz
+go install cuelang.org/go/cmd/cue@latest
+
+echo "The tutorial environment is now ready.\n"
 
 # 'This is a bash shell script.\n\n'
 # 'Any commands you put in here will be executed at runtime.\n\n'
